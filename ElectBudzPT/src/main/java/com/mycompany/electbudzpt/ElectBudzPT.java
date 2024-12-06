c96fe7ecc38116fb79ec735d959271dbec083783
erich-gagui18 <erichjeremiahpaula.gagui@gmail.com>
2024-12-06 19:43:41 +0800

    Second haha

--- ElectBudzPT/src/main/java/com/mycompany/electbudzpt/ElectBudzPT.java
+++ ElectBudzPT/src/main/java/com/mycompany/electbudzpt/ElectBudzPT.java
@@ -11,6 +11,6 @@ package com.mycompany.electbudzpt;
 public class ElectBudzPT {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hi!");
     }
 }
